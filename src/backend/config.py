e2d4493d93c61727a167dd79631171f072821390
taurus5650 <taurus_5650@hotmail.com>
2025-08-07 02:56:03 +0800

    Fix: DB connetor

--- src/backend/config.py
+++ src/backend/config.py
@@ -1,14 +1,30 @@
 import os
 
-IS_CLOUD_RUN = os.getenv('K_SERVICE') is not None  # Cloud Run
+USE_CONNECTOR = os.getenv('USE_CONNECTOR', 'false').lower() == 'true'
 
-if IS_CLOUD_RUN:
-    DB_HOST = os.getenv('DB_HOST', f"/cloudsql/{os.getenv('INSTANCE_CONNECTION_NAME')}")
+if USE_CONNECTOR:
+    from google.cloud.sql.connector import Connector
+    import sqlalchemy
+
+    def getconn():
+        connector = Connector()
+        conn = connector.connect(
+            'terraform-practice-250806:asia-east1:flask-db-instance',
+            'pymysql',
+            user=os.getenv('DB_USER'),
+            password=os.getenv('DB_PASSWORD'),
+            db=os.getenv('DB_NAME'),
+        )
+        return conn
+
+    SQLALCHEMY_DATABASE_URI = sqlalchemy.engine.url.URL.create(
+        drivername='mysql+pymysql',
+        query={"unix_socket": "/cloudsql/terraform-practice-250806:asia-east1:flask-db-instance"},
+    )
+
 else:
-    DB_HOST = os.getenv('DB_HOST', 'cloudsql-proxy')  # Local Dev
-
-DB_USER = os.getenv('DB_USER')
-DB_PASSWORD = os.getenv('DB_PASSWORD')
-DB_NAME = os.getenv('DB_NAME')
-
-SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+    DB_HOST = os.getenv('DB_HOST', '127.0.0.1')
+    DB_USER = os.getenv('DB_USER')
+    DB_PASSWORD = os.getenv('DB_PASSWORD')
+    DB_NAME = os.getenv('DB_NAME')
+    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:3306/{DB_NAME}"
--- src/backend/run.py
+++ src/backend/run.py
@@ -28,3 +28,6 @@ if __name__ == "__main__":
     debug_mode = os.environ.get('FLASK_DEBUG', 'false').lower() == 'true'
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=debug_mode)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+    app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {"connect_args": connect_args}
