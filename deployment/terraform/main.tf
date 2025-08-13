1631586f8baf699287ef83b3644513db1bae5851
taurus5650 <taurus_5650@hotmail.com>
2025-08-13 23:11:38 +0800

    Feat: rm vpc

--- deployment/terraform/main.tf
+++ deployment/terraform/main.tf
@@ -53,7 +53,7 @@ module "mysql" {
   zone                      = var.zone
   tier                      = "db-f1-micro"
   database_version          = "MYSQL_8_0"
-  deletion_protection       = false
+  deletion_protection       = true
   vpc_network               = module.network.network_self_link
   allocated_ip_range        = module.private_service_access.google_compute_global_address_name
   module_depends_on         = [module.private_service_access.peering_completed]
@@ -79,22 +79,6 @@ resource "google_artifact_registry_repository" "repo" {
   location = var.region
   repository_id = "terraform-practice-repo"
   format = "DOCKER"
-}
-
-resource "google_compute_global_address" "google_managed_services_range" {
-  # Keep Private IP
-  name          = "google-managed-services-${var.network_name}"
-  purpose       = "VPC_PEERING"
-  address_type  = "INTERNAL"
-  prefix_length = 16
-  network       = "projects/${var.project_id}/global/networks/${var.network_name}"
-}
-
-resource "google_service_networking_connection" "private_vpc_connection" {
-  # Build VPC Peering
-  network                 = "projects/${var.project_id}/global/networks/${var.network_name}"
-  service                 = "servicenetworking.googleapis.com"
-  reserved_peering_ranges = [google_compute_global_address.google_managed_services_range.name]
 }
 
 # resource "google_project_iam_member" "sa_cloudsql_access" {
